c8e720d27677a7164d8f57d934199cf69c2710cb
gsainaveen9999 <55909524+gsainaveen9999@users.noreply.github.com>
2020-08-14 21:38:11 +0530

    initial

---
+++ Reverse.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Reverse{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+   System.out.println("Enter the String");
+	String a=sc.nextLine();
+	String b="";
+	for(int i=a.length()-1;i>=0;i--){
+     b=b+a.charAt(i);
+		
+	}
+	System.out.println(b);
+	}
+}
